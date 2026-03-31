0845e64e70273323f9c557f2c47137b63e83a6da
=Val242 <=ebongvalentine70@gmail.com>
2026-03-31 06:37:03 +0100

    Creating unique user task and multiple usertask

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,51 @@
-export class CreateTaskDto {}
+import { IsNotEmpty, IsString, IsOptional, IsInt, IsDateString } from "class-validator";
+
+export class CreateTaskDto {
+      @IsString()
+      @IsNotEmpty()
+      title: string;
+    
+      @IsString()
+      @IsOptional()
+      comments?: string;
+    
+      @IsDateString()
+      dueDate: string;
+    
+      @IsInt()
+      @IsNotEmpty()
+      subjectId: number;
+    
+      @IsInt()
+      @IsNotEmpty()
+      classId: number;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// model Task {
+//   id        Int      @id @default(autoincrement())
+//   title     String
+//   dueDate      DateTime
+//   comments  String?
+//   // status Status
+
+//   createdAt DateTime @default(now())
+//   updatedAt DateTime @updatedAt
+
+//   subject   Subject  @relation(fields: [subjectId], references: [id])
+//   subjectId Int
+//   class   Class @relation(fields: [classId], references: [id])
+//   classId Int
+
+//   userTasks UserTask[]
+// }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class TaskService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(private readonly databaseService: DatabaseService){}
+
+ async create(createTaskDto: CreateTaskDto) {
+    const task = await this.databaseService.task.create({
+      data: createTaskDto
+    })
+
+    const users = await this.databaseService.user.findMany({
+    where: {
+      classId: createTaskDto.classId,
+    },
+    select: {
+      id: true,
+    },
+   });
+
+    const userTasksData = users.map((user) => ({
+    userId: user.id,
+    taskId: task.id,
+   
+   }));
+
+  await this.databaseService.userTask.createMany({
+    data: userTasksData,
+  });
+
+  return task;
+
   }
 
   findAll() {
